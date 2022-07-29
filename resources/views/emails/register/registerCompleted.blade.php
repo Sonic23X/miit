97d693f6551303fd081ee6581e0dd9d2c036f5dc
Alfredo Ortega Ramirez <alfredoortega@blue.gen.in>
2022-07-28 22:06:53 -0500

    change to png on email

--- resources/views/emails/register/registerCompleted.blade.php
+++ resources/views/emails/register/registerCompleted.blade.php
@@ -3,7 +3,7 @@
 Muestra el siguiente código para entrar al evento
 
 <div style="display: flex; justify-content:center;">
-    <img src="{{ asset('qrcodes/'. $registration->id .'.svg') }}" alt="qr_code"
+    <img src="{{ asset('qrcodes/'. $registration->id .'.png') }}" alt="qr_code"
         style="width: 50%;">
 </div>
 
