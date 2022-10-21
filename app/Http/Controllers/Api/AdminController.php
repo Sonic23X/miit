f1f0cee4639e537139990826f3f2c8f725139d9b
Alfredo Ortega Ramirez <omar.alfredo49@gmail.com>
2022-10-21 01:47:35 -0500

    Add webhook v1

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -38,5 +38,26 @@ class AdminController extends Controller
 
     public function payment(Request $request) {
         \Log::info($request);
+
+        if ($request->data->type === "charge.paid") {
+            if ($request->data->object->amount === 16000 || $request->data->object->amount === 6000) {
+                $user = Race::where('email', $request->data->object->customer_info->email)->first();
+                $user->payment_mode = Race::MODE_CARD;
+                $user->payment_status = 1;
+
+                $user->save();
+
+                return response()->json([], 200);
+            }
+            else if ($request->data->object->amount === 70000) {
+                $user = Canadevi::where('email', $request->data->object->customer_info->email)->first();
+                $user->payment_mode = Race::MODE_CARD;
+                $user->payment_status = 1;
+
+                $user->save();
+
+                return response()->json([], 200);
+            }
+        }
     }
 }
